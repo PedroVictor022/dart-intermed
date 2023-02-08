01559da0560a32c6d99fe201a837442a9140dfa7
Pedro Victor <pedro20002victor@gmail.com>
2023-02-08 08:08:47 -0300

    async

--- bin/dart_intermediario.dart
+++ bin/dart_intermediario.dart
@@ -1,5 +1,18 @@
-import 'package:dart_intermediario/dart_intermediario.dart' as dart_intermediario;
+// Future, async e await
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_intermediario.calculate()}!');
+void main() async {
+  String name = "Pedro";
+  Future<String> cepFuture = getCepByName("Rua JK");
+  late String cep;
+
+  cepFuture.then((data) => {cep = data, print('$cep -- $data')});
+
+  cep = await cepFuture;
+  print(cep);
+}
+
+// External service
+
+Future<String> getCepByName(String locationName) {
+  return Future.value("00000-000");
 }
