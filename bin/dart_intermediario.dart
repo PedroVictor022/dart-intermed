8a4f2e290ee1b11d2485d0d8fb34277edd0ed48d
Pedro Victor <pedro20002victor@gmail.com>
2023-02-08 07:50:55 -0300

    first commit

---
+++ bin/dart_intermediario.dart
@@ -0,0 +1,5 @@
+import 'package:dart_intermediario/dart_intermediario.dart' as dart_intermediario;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_intermediario.calculate()}!');
+}
